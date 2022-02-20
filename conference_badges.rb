7e84864ff82a5884ef1374bcde71f56fb2e8edee
Julie Evans <juliemevans17@gmail.com>
2022-02-19 19:17:07 -0600

    commit tree a9e6b8305492ca10d8dba10d9eadc5a89b1d1b95

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker name
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+  names.map { |name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms speakers
+  number = 0
+  speakers.map do |name|
+    number = number + 1
+    "Hello, #{name}! You'll be assigned to room #{number}!"
+  end
+end
+
+def printer speakers
+  batch_badge_creator(speakers).map { |printing| puts printing }
+  assign_rooms(speakers).map { |printing| puts printing }
+end
